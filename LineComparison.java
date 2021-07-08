33c1f07f2f31192f06e1210fc316b6d902ba16e8
SakshiShetty641 <sakshishetty1231997@gmail.com>
2021-07-08 22:13:36 +0530

    Added: To find the length  between two points

--- LineComparison.java
+++ LineComparison.java
@@ -1,10 +1,32 @@
 package linecomparison;
+import java.util.Scanner;
 /**
  * @author Sakshi Shetty
  * Purpose - Line Comparison and Computation using OOPS concept
  */
 public class LineComparison {
+
+	/* To find the length of line using co-ordinates provided from user */
+	public double FindLength() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first co-ordinate for x-axis of Line");
+		int x1 = sc.nextInt();
+		System.out.println("Enter the second co-ordinate for x-axis of Line");
+		int x2 = sc.nextInt();
+		System.out.println("Enter the first co-ordinate for y-axis of Line");
+		int y1 = sc.nextInt();
+		System.out.println("Enter the second co-ordinate for y-axis of Line");
+		int y2 = sc.nextInt();
+		sc.close();
+		/* Calculating length between the two points of the Line */
+		double lengthofline = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("The lenth of First Line is :" + lengthofline);
+		return lengthofline;
+	}
+
 	public static void main(String args[]) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+		LineComparison Compare = new LineComparison();
+		Compare.FindLength();
+
 	}
 }
