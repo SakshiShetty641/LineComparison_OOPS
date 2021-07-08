be61d4e7922f997152fc410f82e796e46e504549
SakshiShetty641 <sakshishetty1231997@gmail.com>
2021-07-08 21:59:52 +0530

    Added: To create Line Comparison Program

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+package linecomparison;
+/**
+ * @author Sakshi Shetty
+ * Purpose - Line Comparison and Computation using OOPS concept
+ */
+public class LineComparison {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
